567c39feb5e9be2d7eb7fec4ad6d5c40965329e8
FlexonaFFt <flexonafft@bk.ru>
2024-07-13 23:06:26 +0300

    --load firsttest frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <header className="main">
+      <div className="generative-container">
+        <p className="generative-text">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elit sed diam nonumy
+          eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+          diam voluptua
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 }
 
